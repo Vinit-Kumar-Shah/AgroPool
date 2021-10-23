b89b0de9cc84a5c52e967dc3f8d1b392bf4905e2
Vinit-Kumar-Shah <vinitkumar18072001@gmail.com>
2021-10-23 15:54:59 +0530

    Initialized First Build

---
+++ android/app/src/main/kotlin/com/abhivansh/uberclone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.app.agropool
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+import 'package:agropool/Screens/homescreen.dart';
+import 'package:agropool/Screens/loginscreen.dart';
+import 'package:agropool/Screens/registerScreen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
+import 'package:agropool/appdata.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'methods.dart';
+import 'dart:async';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+DatabaseReference UserReference =
+    FirebaseDatabase.instance.reference().child("users");
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => AppData(),
+      child: MaterialApp(
+        theme: ThemeData(primaryColor: Colors.green),
+        routes: {
+          HomeScreen.ScreenId: (context) => HomeScreen(),
+          LoginScreen.ScreenId: (context) => LoginScreen(),
+          RegistrationScreen.ScreenId: (context) => RegistrationScreen()
+        },
+        debugShowCheckedModeBanner: false,
+        title: 'Agro-Pool',
+        initialRoute: LoginScreen.ScreenId,
+      ),
+    );
+  }
+}
